032d814d10359811b1f9af1dbb705512ed46af4a
Callum <callumccr@gmail.com>
2021-04-23 12:08:22 +0100

    added in humn.ai terragrunt framework & updated ocumentation

--- variables.tf
+++ variables.tf
@@ -1,8 +1,34 @@
+# -----------------------------------------------------------------------------
+# Variables: Common AWS Provider - Autoloaded from Terragrunt
+# -----------------------------------------------------------------------------
+
+variable "aws_region" {
+  description = "The AWS region (e.g. ap-southeast-2). Autoloaded from region.tfvars."
+  type        = string
+  default     = ""
+}
+
+variable "aws_account_id" {
+  description = "The AWS account id of the provider being deployed to (e.g. 12345678). Autoloaded from account.tfvars"
+  type        = string
+  default     = ""
+}
+
+variable "aws_assume_role_arn" {
+  description = "(Optional) - ARN of the IAM role when optionally connecting to AWS via assumed role. Autoloaded from account.tfvars."
+  type        = string
+  default     = ""
+}
+
 variable "deletion_window_in_days" {
   type        = number
   default     = 10
   description = "Duration in days after which the key is deleted after destruction of the resource"
 }
+
+# -----------------------------------------------------------------------------
+# Variables: TF-MOD-AWS-KMS-KEY
+# -----------------------------------------------------------------------------
 
 variable "enable_key_rotation" {
   type        = bool
